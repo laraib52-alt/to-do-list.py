0c2e70e0724755c8bddfbe10a9794416208035ac
laraib52-alt <faisallaraib52@gmail.com>
2025-09-14 15:15:56 +0500

    Add files via upload

---
+++ to do list.py
@@ -0,0 +1,12 @@
+   rules = []
+while True:
+   rule = input("Enter a rule :write clean code")
+   if rule.lower() == "stop":
+      break
+   rules.append(rule)
+print("Enter a rules:")
+for r in rules:
+    print (r)        
+
+
+
